e7eb481fe9b735834bed3ab8453e8e330a4730af
ravindraChoudhary056 <ravindrajakhadh017@gmail.com>
2026-09-05 19:38:59 +0530

    Fix production CORS configuration allow both frontend url local and vercel both

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,8 +9,8 @@ app.use(cookieParser())
 app.use(cors({
     origin: [
         "http://localhost:5173",
-        process.env.FRONTEND_URL
-    ].filter(Boolean),
+        "https://career-lens-ai-ravi.vercel.app"
+    ],
     credentials: true
 }))
 
