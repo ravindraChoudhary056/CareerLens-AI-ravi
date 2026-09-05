48ac163f8dd6259797146e578f9a5bdd38a20ada
ravindraChoudhary056 <ravindrajakhadh017@gmail.com>
2026-09-05 19:00:25 +0530

    Centralize frontend API base URL in Frontend/src/api.js

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
+export const API_BASE_URL = "http://localhost:5000";
+
 const api = axios.create({
-    baseURL: "http://localhost:5000",
+    baseURL: API_BASE_URL,
     withCredentials: true,
 })
 
