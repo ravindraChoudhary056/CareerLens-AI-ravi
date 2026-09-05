70ce41b612e2d990ddb5f68e2e67f35462008540
ravindraChoudhary056 <ravindrajakhadh017@gmail.com>
2026-09-05 19:15:52 +0530

    Change API base URL to production endpoint
    deploy backend on render so replace local backend url to render url

--- Frontend/src/api.js
+++ Frontend/src/api.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-export const API_BASE_URL = "http://localhost:5000";
+export const API_BASE_URL = "https://careerlens-ai-ravi.onrender.com";
 
 const api = axios.create({
     baseURL: API_BASE_URL,
